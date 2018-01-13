3b7dc3a270b156eaba56bcf93fe57a7ccfeda37d
Shital4046 <shital55patil@gmail.com>
2018-01-13 16:27:54 +0530

    Added armodule creation

---
+++ NetConnect-NetChain2/src/main/java/com/connecticus/netchain2/pageObjects/common/arCreation/ARModuleCreation.java
@@ -0,0 +1,16 @@
+package com.connecticus.netchain2.pageObjects.common.arCreation;
+
+import com.connecticus.engine.Common;
+
+public class ARModuleCreation {
+	public void clickARLink() {
+		Common.click("AR_CLICK_ACCOUNT_RECEIVABLE_XPATH");
+		
+	}
+	
+	public void clickNewClientLink() {
+		Common.click("AR_CLICK_NEW_CLIENT_XPATH");
+		
+	}
+
+}
