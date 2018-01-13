d816b630e64b8e74415d21e57651babaa8c06353
CTPL-Ruchira <yrvlande@gmail.com>
2018-01-13 17:39:37 +0530

    Added Custom Exception

---
+++ NetConnect-Test-Framework/src/main/java/com/connecticus/utils/CustomException/KeywordException.java
@@ -0,0 +1,18 @@
+package com.connecticus.utils.CustomException;
+
+public class KeywordException extends RuntimeException {
+	
+	
+	
+	 /**
+	 *Custom Exception
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public KeywordException(String message, Exception e) {
+		 //super(message);
+		   System.out.println(message);
+		     e.printStackTrace();
+	 }
+
+}
