ec1f26a91c885d0ceec6ef06576ea10c1901239f
Shital4046 <shital55patil@gmail.com>
2018-01-29 11:12:01 +0530

    added method for click new client link

---
+++ NetConnect-NetChain2/src/main/java/com/netChain2/selenium/pageObjects/common/arCreation/ARModuleCreation.java
@@ -0,0 +1,15 @@
+package com.netChain2.selenium.pageObjects.common.arCreation;
+
+import com.netChain2.engine.Common;
+
+public class ARModuleCreation {
+	public void clickARLink() {
+		Common.click("AR_CLICK_ACCOUNT_RECEIVABLE_XPATH");
+		
+	}
+	
+	public void clickNewClientLink() {
+		Common.click("AR_CLICK_NEW_CLIENT_XPATH");
+		
+	}
+}
