5832a55c58b3c67712f4a00d891968da6a4185cb
CTPL-Ruchira <yrvlande@gmail.com>
2018-02-03 17:22:32 +0530

    added code make it working

--- NetConnect-Test-Framework/src/main/java/com/netChain2/utils/gmailReader/ReadGmail.java
+++ NetConnect-Test-Framework/src/main/java/com/netChain2/utils/gmailReader/ReadGmail.java
@@ -1,15 +1,17 @@
 package com.netChain2.utils.gmailReader;
 
-import java.io.IOException;
 import java.util.Properties;
+
+import javax.mail.BodyPart;
+import javax.mail.Flags;
 import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.NoSuchProviderException;
 import javax.mail.Session;
 import javax.mail.Store;
 import javax.mail.search.FlagTerm;
-import javax.mail.*;
 
 public class ReadGmail {
 
@@ -17,19 +19,26 @@ public class ReadGmail {
 		Properties props = System.getProperties();
 		props.setProperty("mail.store.protocol", "imaps");
 		String mailContent=null;
+		Store store = null;
+		Folder inbox = null;
 		try {
 			Session session = Session.getDefaultInstance(props, null);
-			Store store = session.getStore("imaps");
+			store = session.getStore("imaps");
 			store.connect("imap.gmail.com", userName, password);
 			System.out.println(store);
 
-			Folder inbox = store.getFolder("Inbox");
+			inbox = store.getFolder("Inbox");
 			inbox.open(Folder.READ_ONLY);
 			Flags seen = new Flags(Flags.Flag.SEEN);
 			FlagTerm unseenFlagTerm = new FlagTerm(seen, false);
 			Message messages[] = inbox.search(unseenFlagTerm);
 
 			for (int readCount = 0; readCount <= 10; readCount++) {
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 				if (messages.length == 0 || !messages[0].getSubject().contains("Netchain2")) {
 					messages = inbox.search(unseenFlagTerm);
 					continue;
@@ -38,8 +47,19 @@ public class ReadGmail {
 			}
 
 			try {
-				mailContent = messages[0].getContent().toString();
-			} catch (IOException e) {
+				
+				Object obj = messages[0].getContent();
+				Multipart mp = (Multipart) obj;
+				BodyPart bp =  mp.getBodyPart(0);
+				mailContent =  bp.getContent().toString();
+				
+				
+                if(store !=null) {
+                	 store.close();
+                 }
+                 
+                 
+			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
@@ -56,5 +76,5 @@ public class ReadGmail {
 		return mailContent;
 
 	}
-
+	
 }
