293adf10a83fc1e4bec2a54c9ea9e83f970f7f56
CTPL-Ruchira <yrvlande@gmail.com>
2018-02-03 18:28:55 +0530

    reading of accept and reject url

--- NetConnect-Test-Framework/src/main/java/com/netChain2/utils/gmailReader/ReadGmail.java
+++ NetConnect-Test-Framework/src/main/java/com/netChain2/utils/gmailReader/ReadGmail.java
@@ -1,6 +1,10 @@
 package com.netChain2.utils.gmailReader;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.mail.BodyPart;
 import javax.mail.Flags;
@@ -21,6 +25,7 @@ public class ReadGmail {
 		String mailContent=null;
 		Store store = null;
 		Folder inbox = null;
+		Message messages[]=null;
 		try {
 			Session session = Session.getDefaultInstance(props, null);
 			store = session.getStore("imaps");
@@ -28,10 +33,10 @@ public class ReadGmail {
 			System.out.println(store);
 
 			inbox = store.getFolder("Inbox");
-			inbox.open(Folder.READ_ONLY);
+			inbox.open(Folder.READ_WRITE);
 			Flags seen = new Flags(Flags.Flag.SEEN);
 			FlagTerm unseenFlagTerm = new FlagTerm(seen, false);
-			Message messages[] = inbox.search(unseenFlagTerm);
+			messages= inbox.search(unseenFlagTerm);
 
 			for (int readCount = 0; readCount <= 10; readCount++) {
 				try {
@@ -51,18 +56,13 @@ public class ReadGmail {
 				Object obj = messages[0].getContent();
 				Multipart mp = (Multipart) obj;
 				BodyPart bp =  mp.getBodyPart(0);
-				mailContent =  bp.getContent().toString();
-				
-				
-                if(store !=null) {
-                	 store.close();
-                 }
-                 
-                 
+				mailContent =  bp.getContent().toString();       
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
+				System.out.println("Mail Has not yrt arrived, May be its late or Its Product Bug");
 				e.printStackTrace();
 			}
+			
+			
 			
 
 		} catch (NoSuchProviderException e) {
@@ -73,8 +73,39 @@ public class ReadGmail {
 		
 		}
 		
+		try {
+			inbox.setFlags(new Message[] {messages[0]}, new Flags(Flags.Flag.SEEN), true);
+			if(store !=null) {
+	        	 store.close();
+	         }
+
+		} catch (MessagingException e) {
+			
+			e.printStackTrace();
+		}
+        		
 		return mailContent;
 
+	}
+	
+	public List<String> getAllUrls(String userName, String password) {
+		String mailContent = readFirstUnreadEmail(userName, password);
+		
+		List<String> containedUrls = new ArrayList<String>();
+	      String urlRegex = "((https?|ftp|gopher|telnet|file):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)";
+	      Pattern pattern = Pattern.compile(urlRegex, Pattern.CASE_INSENSITIVE);
+	      Matcher urlMatcher = pattern.matcher(mailContent);
+
+	      while (urlMatcher.find())
+	      {
+	    	String url =mailContent.substring(urlMatcher.start(0),urlMatcher.end(0));
+	    	 if( url.substring(url.length()-2, url.length()).equals("If")) {
+	    		 url=url.substring(0,url.length()-2);
+	    	 }
+	    	 containedUrls.add(url);
+	      }
+
+	      return containedUrls;
 	}
 	
 }
