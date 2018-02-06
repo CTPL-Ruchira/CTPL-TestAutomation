941a435124fd692428a96e23fad1c587a350900c
CTPL-TestingMK <manoj.kumar@connecticus.in>
2018-02-06 12:38:42 +0530

    updating dispute

--- NetConnect-NetChain2/src/test/java/com/netChain2/selenium/tests/Invoice/ResolveDispute.java
+++ NetConnect-NetChain2/src/test/java/com/netChain2/selenium/tests/Invoice/ResolveDispute.java
@@ -6,7 +6,6 @@ import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import com.netChain2.engine.BaseTestCase;
 import com.netChain2.engine.Common;
 import com.netChain2.selenium.pageObjects.accountsPayable.createInvoice.InvoiceCreationForm;
