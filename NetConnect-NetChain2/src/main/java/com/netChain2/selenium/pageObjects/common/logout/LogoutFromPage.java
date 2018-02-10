33c3eaa735542c595cc260c7f10c503cba382ecc
Shital4046 <shital55patil@gmail.com>
2018-02-10 16:34:05 +0530

    merged

--- NetConnect-NetChain2/src/main/java/com/netChain2/selenium/pageObjects/common/logout/LogoutFromPage.java
+++ NetConnect-NetChain2/src/main/java/com/netChain2/selenium/pageObjects/common/logout/LogoutFromPage.java
@@ -6,7 +6,9 @@ public class LogoutFromPage
 {
 	public static void logout()
 	{
+		Common.sleep(2000);
 		Common.click("NAVIGATION_MENU_PARTIAL_LINK");
+		Common.sleep(2000);
 		Common.click("LOGOUT_BUTTON_XPATH");
 	}
 }
