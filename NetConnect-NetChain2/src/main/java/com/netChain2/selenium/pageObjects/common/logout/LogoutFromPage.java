1dd8d0f4bb0884b1c791b28e701f1ab1db9fe77d
CTPL-Ruchira <yrvlande@gmail.com>
2018-02-12 00:08:46 +0530

    changed

--- NetConnect-NetChain2/src/main/java/com/netChain2/selenium/pageObjects/common/logout/LogoutFromPage.java
+++ NetConnect-NetChain2/src/main/java/com/netChain2/selenium/pageObjects/common/logout/LogoutFromPage.java
@@ -1,14 +1,21 @@
 package com.netChain2.selenium.pageObjects.common.logout;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
 import com.netChain2.engine.Common;
 
 public class LogoutFromPage 
 {
 	public static void logout()
 	{
+		WebElement menuELe= Common.getElement("NAVIGATION_MENU_PARTIAL_LINK");
+		Actions action = new Actions(Common.getDriver());
+		action.click(menuELe).perform();
 		Common.sleep(2000);
-		Common.click("NAVIGATION_MENU_PARTIAL_LINK");
-		Common.sleep(3000);
-		Common.click("LOGOUT_BUTTON_XPATH");
+		action = new Actions(Common.getDriver());
+		WebElement logOutEle = Common.getElement("LOGOUT_BUTTON_XPATH");
+		action.click(logOutEle).perform();
+		Common.sleep(2000);
 	}
 }
