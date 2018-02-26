73c6877e1c5b929dadbfc84dd4d2ff9070c776c6
CTPL-Ruchira <yrvlande@gmail.com>
2018-02-26 20:17:12 +0530

    changed

--- NetConnect-NetChain2/JobBatch.sh
+++ NetConnect-NetChain2/JobBatch.sh
@@ -1,6 +1,6 @@
-cd /NetConnect-Test-Framework
+cd /var/lib/jenkins/workspace/Run_Selenium_NetChain2/NetConnect-Test-Framework
 sh mvn clean install -f pom.xml
-cd ../NetConnect-NetChain2
+cd /var/lib/jenkins/workspace/Run_Selenium_NetChain2/NetConnect-NetChain2
 sh mvn clean install -f pom.xml
 sh mvn exec:java -f pom.xml
 
