89010b0021549413a66a366b3a55b54be88f9187
CTPL-Ruchira <yrvlande@gmail.com>
2018-02-26 20:04:45 +0530

    modified

--- NetConnect-NetChain2/JobBatch.sh
+++ NetConnect-NetChain2/JobBatch.sh
@@ -1,6 +1,6 @@
-cd NetConnect-Test-Framework
+cd /NetConnect-Test-Framework
 call mvn clean install -f pom.xml
-cd ..\NetConnect-NetChain2
+cd ../NetConnect-NetChain2
 call mvn clean install -f pom.xml
 call mvn exec:java -f pom.xml
 
