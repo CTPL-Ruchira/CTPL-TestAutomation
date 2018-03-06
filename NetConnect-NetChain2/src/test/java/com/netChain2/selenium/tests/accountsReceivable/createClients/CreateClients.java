dd9d9f7baddf055ea8f4a13d2dd991a4c672cfe6
Shital4046 <shital55patil@gmail.com>
2018-03-06 12:37:04 +0530

    Update testfile for create client

--- NetConnect-NetChain2/src/test/java/com/netChain2/selenium/tests/accountsReceivable/createClients/CreateClients.java
+++ NetConnect-NetChain2/src/test/java/com/netChain2/selenium/tests/accountsReceivable/createClients/CreateClients.java
@@ -8,8 +8,9 @@ import org.testng.annotations.Test;
 
 import com.netChain2.engine.BaseTestCase;
 import com.netChain2.engine.Common;
+import com.netChain2.selenium.pageObjects.accountsPayable.createInvoice.InvoiceCreationListActions;
 import com.netChain2.selenium.pageObjects.accountsReceivable.createClients.ClientsCreationForm;
-import com.netChain2.selenium.pageObjects.common.arCreation.ARModuleCreation;
+import com.netChain2.selenium.pageObjects.common.components.CommonMethods;
 import com.netChain2.selenium.pageObjects.common.loginPage.LoginPage;
 import com.netChain2.selenium.pageObjects.common.logout.LogoutFromPage;
 import com.netChain2.utils.CustomAnnotation.TestDetails;
@@ -17,7 +18,9 @@ import com.netChain2.utils.CustomAnnotation.TestDetails;
 	public class CreateClients {
 		private ArrayList<String> testData;
 		private ArrayList<String> testDataClients;
-		private String expectedClientName;
+		private String clientName;
+		ClientsCreationForm arClients=new ClientsCreationForm();
+		InvoiceCreationListActions action=new InvoiceCreationListActions();
 		
 		@BeforeClass
 		public void setUp() {
@@ -27,104 +30,25 @@ import com.netChain2.utils.CustomAnnotation.TestDetails;
 		 @Test
 	     @TestDetails(author="Shital.Patil", description="This method will create a new client from Account receivable > New client")
 		 
-		public void createARClients() {
-			
+		public void createARClients() 
+		 {
 			LoginPage loginPage = new LoginPage();
 			loginPage.login(testData.get(6), testData.get(7));
-			Common.sleep(2000);
+	
+			//click on new clinet link
+			CommonMethods.gotoRightSideARLink("NEW CLIENT");
 			
-			ClientsCreationForm arClients=new ClientsCreationForm();
-			ARModuleCreation arModule = arClients.createNew();
-			Common.sleep(2000);
+            clientName=Common.generateRandomString(testDataClients.get(17));
 			
-			//click to AR()
-			arModule.clickARLink();
+			arClients.createClientMethod(testDataClients.get(0), testDataClients.get(1), testDataClients.get(2), testDataClients.get(3), testDataClients.get(4), testDataClients.get(5), testDataClients.get(6), testDataClients.get(7), testDataClients.get(8), testDataClients.get(9), testDataClients.get(10), testDataClients.get(11), testDataClients.get(12), testDataClients.get(13),testDataClients.get(14), testDataClients.get(15), testDataClients.get(16),clientName , testDataClients.get(18), testDataClients.get(19), testDataClients.get(20),testDataClients.get(21), testDataClients.get(22), testDataClients.get(23), testDataClients.get(24), testDataClients.get(25), testDataClients.get(26), testDataClients.get(27),testDataClients.get(28), testDataClients.get(29), testDataClients.get(30), testDataClients.get(31), testDataClients.get(32), testDataClients.get(33));
 			
-			//Click to New Client
-			arModule.clickNewClientLink();
+			//verify client in list 
+			//search By client Name
+			action.searchInvoice(clientName);
 			
-			//set Company profile Values
-			arClients.setCompanyProfile(testDataClients.get(0), testDataClients.get(1), testDataClients.get(2));
-			
-			//click next button
-			arClients.clickCompanyProfileNextButton();
-			Common.sleep(3000);
-			//click Add Employee Button
-			arClients.clickAddEmpButton();
-				
-			//set Associate Employee values
-			arClients.EnterName(testDataClients.get(3));
-			Common.sleep(2000);
-			arClients.EnterEmail(testDataClients.get(4));
-			arClients.EnterPhoneNumber(testDataClients.get(5));
-			arClients.EnterTitle(testDataClients.get(6));
-			arClients.selectPaymentReceiver();
-			Common.sleep(2000);
-			
-			//click Associate emplyoee Next Button
-			arClients.clickAssociateEmpNextButton();
-			
-			//click Booking Account Add Line Button
-			arClients.clickBookingAccntAddLineButton();
-			
-			//Select Location,Department,Booking Account 
-			arClients.SetBookingAccount(testDataClients.get(7), testDataClients.get(8), testDataClients.get(9));
-			
-			//Click on next Button
-			arClients.clickBookingAccountNextButton();
-			
-			//Transaction Tab 
-			arClients.transactionDetails(testDataClients.get(10), testDataClients.get(11), testDataClients.get(12));
-			Common.sleep(2000);
-			
-			arClients.clickTransDetailNextButton();
-			
-			//Set Client Details
-			expectedClientName=Common.generateRandomString(testDataClients.get(17));
-			arClients.setClientIdentity(testDataClients.get(13), testDataClients.get(14), testDataClients.get(15), testDataClients.get(16),expectedClientName,testDataClients.get(18), testDataClients.get(19), testDataClients.get(20));
-			Common.sleep(2000);
-			
-			arClients.clickPrintOnCheck();
-			Common.sleep(2000);
-			
-			//set Billing Address
-			arClients.setBillingAddress(testDataClients.get(21), testDataClients.get(22), testDataClients.get(23), testDataClients.get(24));
-			Common.sleep(2000);
-			
-			arClients.clickSameAsBillingAddress();
-			Common.sleep(2000);
-			
-			//Set Contact Details
-			String ExpectedEmail=testDataClients.get(25);
-			arClients.setContactDetails(ExpectedEmail, testDataClients.get(26), testDataClients.get(27), testDataClients.get(28), testDataClients.get(29), testDataClients.get(30));
-			Common.sleep(2000);
-			
-			arClients.SelectCompanyTire(testDataClients.get(31));
-			Common.sleep(2000);
-			
-			arClients.clickInvoiceDeliveryMethod();
-			arClients.setEmailOnInvoiceDelivery(testDataClients.get(32));
-			Common.sleep(2000);
-			
-			arClients.setNotes(testDataClients.get(33));
-			Common.sleep(2000);
-		
-			arClients.clickSaveButton();
-			Common.sleep(5000);
-		
-			//Verify Title And ckeck client in present or not
-			String ActualTitleValue=Common.getDriver().getTitle();		
-			String ExpectedTitleValue=testDataClients.get(34);
-			
-			boolean istitleCorrect=arClients.verifyTitleMatched(ActualTitleValue, ExpectedTitleValue);
-			BaseTestCase.assertTrue(istitleCorrect, "Redirected to Client list,client created succesfully");
-					
-			Boolean status=arClients.verifyClientOnList(expectedClientName);
+			Boolean status=arClients.verifyClientOnList(clientName);
 			BaseTestCase.assertTrue(status, "Client not created");
-			Reporter.log("Client created successfully",true);
-			Common.sleep(6000);
+			Common.sleep(5000);
 			LogoutFromPage.logout();
 		 }
-		 
-
 }
