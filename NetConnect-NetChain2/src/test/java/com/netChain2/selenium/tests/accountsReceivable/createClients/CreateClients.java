0029f8de814ac2dbda8d5902fadab2b665b33ab9
Shital4046 <shital55patil@gmail.com>
2018-03-14 19:01:56 +0530

    update Client test

--- NetConnect-NetChain2/src/test/java/com/netChain2/selenium/tests/accountsReceivable/createClients/CreateClients.java
+++ NetConnect-NetChain2/src/test/java/com/netChain2/selenium/tests/accountsReceivable/createClients/CreateClients.java
@@ -39,8 +39,9 @@ import com.netChain2.utils.CustomAnnotation.TestDetails;
 			CommonMethods.gotoRightSideARLink("NEW CLIENT");
 			
             clientName=Common.generateRandomString(testDataClients.get(17));
-			
-			arClients.createClientMethod(testDataClients.get(0), testDataClients.get(1), testDataClients.get(2), testDataClients.get(3), testDataClients.get(4), testDataClients.get(5), testDataClients.get(6), testDataClients.get(7), testDataClients.get(8), testDataClients.get(9), testDataClients.get(10), testDataClients.get(11), testDataClients.get(12), testDataClients.get(13),testDataClients.get(14), testDataClients.get(15), testDataClients.get(16),clientName , testDataClients.get(18), testDataClients.get(19), testDataClients.get(20),testDataClients.get(21), testDataClients.get(22), testDataClients.get(23), testDataClients.get(24), testDataClients.get(25), testDataClients.get(26), testDataClients.get(27),testDataClients.get(28), testDataClients.get(29), testDataClients.get(30), testDataClients.get(31), testDataClients.get(32), testDataClients.get(33));
+        	String email=CommonMethods.generateRandomStringForEmail(testDataClients.get(25));
+        	
+			arClients.createClientMethod(testDataClients.get(0), testDataClients.get(1), testDataClients.get(2), testDataClients.get(3), testDataClients.get(4), testDataClients.get(5), testDataClients.get(6), testDataClients.get(7), testDataClients.get(8), testDataClients.get(9), testDataClients.get(10), testDataClients.get(11), testDataClients.get(12), testDataClients.get(13),testDataClients.get(14), testDataClients.get(15), testDataClients.get(16),clientName , testDataClients.get(18), testDataClients.get(19), testDataClients.get(20),testDataClients.get(21), testDataClients.get(22), testDataClients.get(23), testDataClients.get(24), email, testDataClients.get(26), testDataClients.get(27),testDataClients.get(28), testDataClients.get(29), testDataClients.get(30), testDataClients.get(31), testDataClients.get(32), testDataClients.get(33));
 			
 			//verify client in list 
 			//search By client Name
